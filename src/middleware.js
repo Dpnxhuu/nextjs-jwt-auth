e55301e982f074bed58c4488b0c5482091b96291
dpnxhuu <dpnxhuu@email.com>
2026-05-26 15:03:37 +0530

    fixed

---
+++ src/middleware.js
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server'
+
+const AUTH_PAGES = new Set(['/login', '/signup', '/signup/email'])
+
+export function middleware(request) {
+  const token = request.cookies.get('token')?.value
+  const { pathname } = request.nextUrl
+
+  const isAuthPage = AUTH_PAGES.has(pathname)
+  const isProtected = pathname === '/home' || pathname.startsWith('/home/')
+
+  if (isProtected) {
+    if (!token) {
+      const loginUrl = request.nextUrl.clone()
+      loginUrl.pathname = '/login'
+      return NextResponse.redirect(loginUrl)
+    }
+    return NextResponse.next()
+  }
+
+  if (isAuthPage) {
+    if (token) {
+      const homeUrl = request.nextUrl.clone()
+      homeUrl.pathname = '/home'
+      return NextResponse.redirect(homeUrl)
+    }
+    return NextResponse.next()
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/home',
+    '/home/:path*',
+    '/login',
+    '/signup',
+    '/signup/email',
+  ],
+}
